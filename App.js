31cd50836a7283a3e90166ff71d463167664dd20
AJITHA K M <84003748+AJITHA19104@users.noreply.github.com>
2021-08-17 14:16:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Header';
+import Dam from './Dam';
+
+function App() {
+
+ return (
+    <div className="App">
+     <Header/>
+     <Dam/>
+    </div>
+  );
+}
+
+export default App;
