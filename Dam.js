83b6614a8786b151496afa813e64ffe2a4fac0b9
AJITHA K M <84003748+AJITHA19104@users.noreply.github.com>
2021-08-17 14:16:00 +0530

    Add files via upload

---
+++ Dam.js
@@ -0,0 +1,16 @@
+function Dam(){
+    let name='AADI';
+    return(
+        <div>
+            <h1>GOOD MORNING {name} </h1>
+            <p>
+            Welcome to the team! We are thrilled to have you at our office. <br></br>
+            You're going to be a valuable asset to our company, and we can't
+             wait to see all that you accomplish. The entire team is thrilled
+              to welcome you on board. We hope you'll do some amazing works here!
+            </p>
+        </div>
+    )
+    
+}
+export default Dam;
